883c15a762a558af2612f3f0e9d6faf3076f265f
Yasinehsan <yasinehsan11@gmail.com>
2019-03-22 22:18:36 -0400

    created movieCell

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -18,6 +18,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -28,7 +31,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
-                self.movies = [dataDictionary["results"] as! [String: Any]]
+                self.movies = dataDictionary["results"] as! [[String: Any]]
+                self.tableView.reloadData()
                 print(dataDictionary)
                 
                 // TODO: Get the array of movies
@@ -46,13 +50,18 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50;
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
-        cell.textLabel?.text = "row: \(indexPath.row)"
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        
+        cell.titleLabel!.text = title
+        cell.synopsisLabel.text = synopsis
         
         return cell
     }
